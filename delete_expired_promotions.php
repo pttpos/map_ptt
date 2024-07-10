088c9c293d4507323e97252d7c2c1ba27d5c5f33
pttpos <167395330+pttpos@users.noreply.github.com>
2024-07-10 21:51:12 +0700

    Updated all files in directory 2024-07-10 16:48:03

--- delete_expired_promotions.php
+++ delete_expired_promotions.php
@@ -1,3 +1,22 @@
+<<<<<<< HEAD
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['clear_all_expired'])) {
+    date_default_timezone_set('Asia/Phnom_Penh');
+    $promotions = json_decode(file_get_contents('./data/promotions.json'), true);
+    $current_time = new DateTime('now', new DateTimeZone('Asia/Phnom_Penh'));
+
+    foreach ($promotions['PROMOTIONS'] as &$station) {
+        $station['promotions'] = array_filter($station['promotions'], function ($promo) use ($current_time) {
+            $end_time = new DateTime($promo['end_time']);
+            return $end_time >= $current_time;
+        });
+    }
+
+    file_put_contents('./data/promotions.json', json_encode($promotions, JSON_PRETTY_PRINT));
+    echo 'Expired promotions deleted.';
+}
+?>
+=======
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['clear_all_expired'])) {
     date_default_timezone_set('Asia/Phnom_Penh');
@@ -16,3 +35,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['clear_all_expired']))
 }
 ?>
 
+>>>>>>> 16b83a41566d16625091a05c4a54a63d4d6efe88
